3c6244392917eeddd82b2468429c4327f7e93e83
Harshit <ht2995@gmail.com>
2018-09-09 17:22:27 +0530

    Update main.java

--- SuperClass Manipulation/main.java
+++ SuperClass Manipulation/main.java
@@ -2,7 +2,7 @@ import javassist.*;
 import foo.foo;
 import foo.foo1;
 
-public class jloader {
+public class main {
   public static void main(String[] args) throws Throwable {
      ClassPool pool = ClassPool.getDefault();
      Loader cl = new Loader(pool);
