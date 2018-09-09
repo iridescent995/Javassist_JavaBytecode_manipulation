f9673b0287ec6b47394309b409c4e01ee6d05957
Harshit <ht2995@gmail.com>
2018-09-09 17:08:18 +0530

    super class manipulation

---
+++ SuperClass Manipulation/foo/foo.java
@@ -0,0 +1,10 @@
+package foo;
+public class foo {
+//    public foo() {
+//        System.out.println("foo hello");
+//    }
+    public static void main(String[] args) {
+		System.out.println("hello");
+		//foo f =new foo();
+	}
+}
---
+++ SuperClass Manipulation/foo/foo1.java
@@ -0,0 +1,6 @@
+package foo;
+public class foo1 {
+    public foo1() {
+        System.out.println("foo1");
+    }
+}
