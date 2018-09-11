07d00502ab7e7bb29d5d56926a55c8378ca9ab86
Harshit <ht2995@gmail.com>
2018-09-11 11:38:30 +0530

    comments

--- Method Manipulation/jassist.java
+++ Method Manipulation/jassist.java
@@ -4,8 +4,11 @@ import javassist.*;
 public class jassist {
     public static void main(String[] args) throws Exception {
         ClassPool cp = ClassPool.getDefault();
+        //get the hello class
         CtClass cc = cp.get("Hello");
+        //get the say method
         CtMethod m = cc.getDeclaredMethod("say");
+        //remove by renaming the method
         m.setName("dump");
         
         
@@ -14,14 +17,14 @@ public class jassist {
 //        CtMethod f= ff.getDeclaredMethod("say");
         //m.insertBefore("{ System.out.println(\"Hello.say():\"); }");
         
-        
+        //making a new method with same name
         
         CtMethod method = CtNewMethod.make(
                 "public void say() { int a=1,b=2; System.out.println(\"1 + 2 = \" + a*b); }",
                 cc);
                
         cc.addMethod(method);
-        
+       
         
         Class c = cc.toClass();
         Hello h = (Hello)c.newInstance();
